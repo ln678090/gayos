3c610ea358f2dc4092912ee857c03b301b4791e9
ln678090 <ln678090@gmail.com>
2026-09-02 02:29:43 +0700

    build: limit compression threads for lower thermals

--- profiledef.sh
+++ profiledef.sh
@@ -16,9 +16,15 @@ bootmodes=('bios.syslinux'
 pacman_conf="pacman.conf"
 
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
-
-bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
+#airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+airootfs_image_tool_options=(
+  "-comp" "zstd"
+  "-Xcompression-level" "8"
+  "-b" "1M"
+  "-processors" "4"
+)
+#bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
+bootstrap_tarball_compression=('zstd' '-c' '-T4' '-8')
 airootfs_postprocess_script="airootfs/root/customize_airootfs.sh"
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
